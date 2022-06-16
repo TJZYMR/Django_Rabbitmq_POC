138a0391379370f24c6fe2861bc7470cb414a212
TJZYMR <tatva.joshi@zymr.com>
2022-06-16 17:48:22 +0530

    Initial commit

---
+++ report.py
@@ -0,0 +1,29 @@
+import pika
+import json
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+queue = channel.queue_declare('order_report')
+queue_name = queue.method.queue
+channel.queue_bind(exchange='order',
+                   queue=queue_name,
+                   routing_key='order.report'  # binding key
+                   )
+
+
+def callback(ch, method, properties, body):
+    payload = json.loads(body)
+    print(' [x] generating report')
+    print(f"""
+    ID: {payload.get('id')}
+    User Email: {payload.get('user_email')} 
+    Product: {payload.get('product')} 
+    Quantity: {payload.get('quantity')}
+    """)
+    print('[x] Done')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_consume(on_message_callback=callback, queue=queue_name)
+print('[*] waiting for notify messages.To exit pres Ctril c')
+channel.start_consuming()
